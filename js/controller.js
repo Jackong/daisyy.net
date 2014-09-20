605cf9eec236b92d9ca5e2fed1418d5c47ae2842
Jackong <JackongC@gmail.com>
2014-09-20 14:01:38 +0800

    update tool page

--- js/controller.js
+++ js/controller.js
@@ -3,27 +3,26 @@
  */
 angular.module('daisy.controller', [])
     .controller('AppCtrl', function ($scope) {
-        $scope.nav = function (which) {
+         $scope.nav = function (which) {
             $scope.active = {};
             $scope.active[which] = 'active';
         };
-        $scope.nav('home');
     })
     .controller('HomeCtrl', function ($scope) {
-
+        $scope.$parent.nav('home');
     })
     .controller('ToolsCtrl', function ($scope) {
-
+        $scope.$parent.nav('tools');
     })
     .controller('GamesCtrl', function ($scope) {
-
+        $scope.$parent.nav('games');
     })
     .controller('AppsCtrl', function ($scope) {
-
+        $scope.$parent.nav('apps');
     })
     .controller('AboutCtrl', function ($scope) {
-
+        $scope.$parent.nav('about');
     })
     .controller('ContactCtrl', function ($scope) {
-
+        $scope.$parent.nav('contact');
     });
